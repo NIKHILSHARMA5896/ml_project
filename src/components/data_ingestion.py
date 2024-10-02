ebb6fb0dea16f9634ada16692ba4e503857a04fc
B Nikhil Sharma <b.nikhil.sharma@gmail.com>
2024-10-02 18:12:19 +0530

    Added Data Transformation Script

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from src.logger import logging
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+
 @dataclass
 class DataIngestionConfig:
     train_data_path : str = os.path.join("artifacts","train.csv")
@@ -46,4 +48,9 @@ class DataIngestion:
 
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingesiton()
+    train_data,test_data = obj.initiate_data_ingesiton()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
+
+
